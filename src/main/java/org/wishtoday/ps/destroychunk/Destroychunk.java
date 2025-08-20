ec777026df9100dfa19f48ef2ac31e7c2e5ecd6f
wishtoday238 <wishtoday238@gmail.com>
2025-08-21 01:12:32 +0800

    1

--- src/main/java/org/wishtoday/ps/destroychunk/Destroychunk.java
+++ src/main/java/org/wishtoday/ps/destroychunk/Destroychunk.java
@@ -1,10 +1,20 @@
 package org.wishtoday.ps.destroychunk;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import org.wishtoday.ps.destroychunk.Task.CounterTask;
+import org.wishtoday.ps.destroychunk.Task.TaskManager;
 
 public class Destroychunk implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        ServerTickEvents.END_SERVER_TICK.register(server -> {
+            TaskManager.getInstance().tickTasks();
+        });
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+           TaskManager.getInstance().addTask(new CounterTask(() -> System.out.println("5秒"), 100, false));
+        });
     }
 }
--- src/main/java/org/wishtoday/ps/destroychunk/mixin/LivingEntityMixin.java
+++ src/main/java/org/wishtoday/ps/destroychunk/mixin/LivingEntityMixin.java
@@ -1,9 +1,12 @@
 package org.wishtoday.ps.destroychunk.mixin;
 
+import com.google.common.collect.Lists;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
@@ -12,33 +15,38 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.wishtoday.ps.destroychunk.Task.ClearBlocksTask;
+import org.wishtoday.ps.destroychunk.Task.TaskManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-    @Inject(method = "damage",at = @At("HEAD"))
+    @Inject(method = "damage", at = @At("RETURN"))
     private void onDamage(DamageSource source
             , float amount, CallbackInfoReturnable<Boolean> cir) {
         LivingEntity livingEntity = (LivingEntity) (Object) this;
         if (!(livingEntity instanceof PlayerEntity player)) return;
         World world = player.getWorld();
+        if (world.isClient()) return;
         Chunk chunk = world.getChunk(player.getBlockPos());
         ChunkPos pos = chunk.getPos();
         int startX = pos.getStartX();
-        System.out.println(startX);
         int endY = world.getBottomY();
         int startZ = pos.getStartZ();
-        System.out.println(startZ);
         int endX = pos.getEndX();
-        System.out.println(endX);
         int topY = world.getTopY();
         int endZ = pos.getEndZ();
-        System.out.println(endZ);
-        for (int x = startX; x <= endX; x++) {
-            for (int z = startZ; z <= endZ; z++) {
-                for (int y = endY; y <= topY; y++) {
-                    world.setBlockState(new BlockPos(x,y,z), Blocks.AIR.getDefaultState());
+        List<BlockPos> list = new ArrayList<>();
+        for (int y = topY; y >= endY; y--) {
+            for (int x = startX; x <= endX; x++) {
+                for (int z = startZ; z <= endZ; z++) {
+                    if (world.getBlockState(new BlockPos(x, y, z)).getBlock() == Blocks.AIR) continue;
+                    list.add(new BlockPos(x, y, z));
                 }
             }
         }
+        TaskManager.getInstance().addTask(new ClearBlocksTask((ServerWorld) world, 100 * 100, list));
     }
 }
